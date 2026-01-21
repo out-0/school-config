f7659b7da34296dd01c68e57ba02ae58a71d1d3c
out-0 <ayoubarid000@gmail.com>
2026-01-21 03:04:45 +0100

    automate the backup

---
+++ restore_backup_from_usb.sh
@@ -0,0 +1,1 @@
+rsync -avPH /media/aarid/MY_USB/goinfre_backup/ /goinfre/aarid/
---
+++ take_backup_to_usb.sh
@@ -0,0 +1,3 @@
+mkdir -p /media/aarid/MY_USB/goinfre_backup
+
+rsync -avPH --delete /goinfre/aarid/ /media/aarid/MY_USB/goinfre_backup/
