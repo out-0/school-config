9449aef92fb6b69430ae5bbcf7cc496f1b456651
out-0 <ayoubarid000@gmail.com>
2026-01-24 03:05:05 +0100

    Mapping a good wallpapers to the themes

--- change_kitty_themes.sh
+++ change_kitty_themes.sh
@@ -7,6 +7,7 @@ THEME_FILE="$HOME/school-config/kitty_themes.txt"
 if [ -f "$THEME_FILE" ]; then
     # 1. Pick a random theme
     CHOSEN_THEME=$(shuf -n 1 "$THEME_FILE")
+	echo $CHOSEN_THEME > ~/.config/kitty/current_theme_name.txt
     
     # 2. Apply Kitty Theme
     kitten themes --reload-in=all "$CHOSEN_THEME"
@@ -14,14 +15,30 @@ if [ -f "$THEME_FILE" ]; then
     # 3. Match Wallpaper to Theme
     case "$CHOSEN_THEME" in
         "Cyberpunk Neon")
-            WALLPAPER="neon.jpg"
+            WALLPAPER="Cyberpunk_Neon.jpg"
             ;;
+		"Cyberpunk")
+            WALLPAPER="Cyberpunk.jpg"
+			;;
+		"Pastel Cyberpunk")
+            WALLPAPER="Pastel_Cyberpunk.png"
+			;;
         "Sakura Night")
-            WALLPAPER="sakura_trees.png"
+            WALLPAPER="Sakura_Night.jpg"
             ;;
+        "Duotone Dark")
+            WALLPAPER="Duotone_Dark.jpg"
+			;;
         "1984 Dark")
             WALLPAPER="shinobo.png"
             ;;
+        "shadotheme")
+            WALLPAPER="shadotheme.png"
+            ;;
+        "Tropical Neon")
+            WALLPAPER="Tropical_Neon.jpg"
+            ;;
+
         *)
             # Default wallpaper if theme isn't specifically mapped
             WALLPAPER="default_cyber.png"
@@ -33,13 +50,16 @@ if [ -f "$THEME_FILE" ]; then
     WP_PATH="$WP_DIR/$WALLPAPER"
 
     # 4. Apply to Xresources (for ft_lock)
-    if [ -f "$WP_PATH" ]; then
-        echo "ft_lock.image_file: $WP_PATH" | xrdb -merge
-    fi
+    #if [ -f "$WP_PATH" ]; then
+    #    echo "ft_lock.image_file: $WP_PATH" | xrdb -merge
+    #fi
 	
     # 5. Apply to GNOME Desktop (Optional - if you want desktop bg to match too)
-    if [ -f "$WP_PATH" ]; then
-        gsettings set org.gnome.desktop.background picture-uri "file://$WP_PATH"
-        gsettings set org.gnome.desktop.background picture-uri-dark "file://$WP_PATH"
-    fi
+    #if [ -f "$WP_PATH" ]; then
+    #    gsettings set org.gnome.desktop.background picture-uri "file://$WP_PATH"
+    #    gsettings set org.gnome.desktop.background picture-uri-dark "file://$WP_PATH"
+    #fi
+
+    gsettings set org.gnome.desktop.background picture-uri "file://$WP_PATH"
+    gsettings set org.gnome.desktop.background picture-uri-dark "file://$WP_PATH"
 fi
